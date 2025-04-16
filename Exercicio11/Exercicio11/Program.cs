645b095e2d1457e803964689f67a9f0880ba6782
Joao Gabriel <123424925+artesssn@users.noreply.github.com>
2025-04-16 16:40:14 -0300

    Add files via upload

---
+++ Exercicio11/Exercicio11/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Fila
+{
+    class Program
+    {
+        static void Main()
+        {
+            MinhaFila<int> fila = new MinhaFila<int>();
+
+            fila.Enqueue(1);
+            fila.Enqueue(2);
+            fila.Enqueue(3);
+            fila.Enqueue(4);
+            fila.Enqueue(5);
+
+            Console.WriteLine("Total de itens na fila: " + fila.Count());
+
+            Console.WriteLine("Elemento do topo da fila: " + fila.Peek());
+
+            Console.WriteLine("Removendo o elemento do topo da fila: " + fila.Dequeue());
+
+            Console.WriteLine("Total de itens na fila: " + fila.Count());
+        }
+    }
+
+    class MinhaFila<T>
+    {
+        private List<T> fila = new List<T>();
+        public void Enqueue(T item) => fila.Add(item);
+
+        public T Dequeue()
+        {
+            if (fila.Count == 0)
+                throw new InvalidOperationException("A fila estaazia.");
+            T item = fila[0];
+            fila.RemoveAt(0);
+            return item;
+        }
+
+        public T Peek()
+        {
+            if (fila.Count == 0)
+                throw new InvalidOperationException("A fila est vazia.");
+            return fila[0];
+        }
+
+        public int Count() => fila.Count;
+    }
+}
---
+++ Exercicio12/Exercicio12/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinarioParaDecimal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> pilha = new Stack<int>();
+
+            Console.WriteLine("Digite um número decimal: ");
+            int numeroDecimal = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Número binário: ");
+
+            while (numeroDecimal > 0)
+            {
+                int resto = numeroDecimal % 2;
+                pilha.Push(resto);
+                numeroDecimal /= 2;
+            }
+
+            while (pilha.Count > 0)
+            {
+                Console.WriteLine(pilha.Pop());
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
